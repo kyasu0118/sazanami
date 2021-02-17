95061b22e82ed3d8261cf514db796fee332a0fb6
kyasu0118 <kyasu0118@gmail.com>
2021-02-18 01:52:00 +0900

    add workspace. update require.php, Json.class

--- require.php
+++ require.php
@@ -1,108 +1,165 @@
 <?php
 foreach ( glob( __DIR__ . "/class/*.class" ) as $filePath )
 {
-  $class = "namespace sazanami;";
-  $className = "";
-  $memberVariable = [];
-  $localVariable = [];
-  
-  foreach (file($filePath) as &$value) 
-  {
-      $words = preg_split("/[\s,():;]+/", $value);
-      $words = array_filter($words, function($value){
-          return empty($value) == false || $value === '0' || $value === 0;
-      });
-      $words = array_values($words);
-      $wordsLength = count($words);
-  
-      if( $wordsLength > 0 )
-      {
-          switch($words[0])
-          {                
-              case "class":
-                  $className = $words[1];
-                  $class .= $value;
-                  break;
-              case "var":
-                  $localVariable[] = $words[1];
-                  $class .= "$" . $words[1];
-                  for ($i = 2; $i <$wordsLength; $i++)
-                  {
-                      $class .= $words[$i];
-                  }
-                  $class .= ";";
-                  break;
-              case "public":
-              case "protected":
-              case "private":
-                  $header = $words[0];
-                  if( strcmp($words[1], "static") == 0 )
-                  {
-                      $header .= " static";
-                      unset($words[1]);
-                      $words = array_values($words);
-                      $wordsLength--;
-                  }
-                  // member variable.
-                  if( strcmp($words[1], "var") == 0 )
-                  {
-                      $memberVariable[] = $words[2];
+    $class = "namespace sazanami;";
+    $className = "";
+    $memberVariable = [];
+    $localVariable = [];
 
-                      $value = preg_replace(
-                        '/([\s,();=])('.$words[2].')([\s,();=])/', "$1 $" . $words[2]. " $3", $value);
+    foreach (file($filePath) as &$value) 
+    {
+        $value = str_replace("(", " ( ", $value);
+        $value = str_replace(")", " ) ", $value);
+        $value = str_replace("{", " { ", $value);
+        $value = str_replace("}", " } ", $value);
+        $value = str_replace(",", " , ", $value);
+        $value = str_replace("=", " = ", $value);
+        $value = str_replace("-", " - ", $value);
+        $value = str_replace("- >", " -> ", $value);
+        $value = str_replace(";", " ; ", $value);
+        $value = str_replace("+", " + ", $value);
+        $value = str_replace("*", " * ", $value);
+        $value = str_replace("/", " / ", $value);
+        $words = preg_split("/[\s]+/", $value);
+        $words = array_filter($words, function($value){
+            return empty($value) == false || $value === '0' || $value === 0;
+        });
+        $words = array_values($words);
+        $wordsLength = count($words);
 
-                      $value = preg_replace(
-                        '/([\s,();=])(var)([\s,();=])/', "", $value);
+        if( $wordsLength > 0 )
+        {
+            $index = 0;
+            switch($words[0])
+            {                
+                case "class":
+                    $className = $words[1];
+                    $class .= $value;
+                    break;
+                case "public":
+                case "protected":
+                case "private":
+                    $class .= $words[0] . " ";
+                    $index = 1;
+                    if( strcmp($words[1], "static") == 0 )
+                    {
+                        $class .= "static";
+                        $index++;
+                    }
+                    // member variable.
+                    if( strcmp($words[$index], "var") == 0 )
+                    {
+                        $index++;
+                        $memberVariable[] = $words[$index];
+                        $class .= "$" . $words[$index];
+                        $index++;      
+                       
+                        for ($i = $index; $i <$wordsLength; $i++)
+                        {
+                            $doll = "";
+                            for ($j = 0; $j<count($localVariable); $j++)
+                            {
+                                if( strcmp($words[$i],$localVariable[$j]) == 0 )
+                                {
+                                    $doll = "$";
+                                    break;
+                                }
+                            }
+                            if( strcmp($doll, "") == 0 )
+                            { 
+                                for ($j = 0; $j<count($memberVariable); $j++)
+                                {
+                                    if( strcmp($words[$i],$memberVariable[$j]) == 0 )
+                                    {
+                                        $doll = '$this';
+                                        break;
+                                    }
+                                }
+                            }
+                            $class .= $doll . $words[$i];
+                        }
+                    }
+                    // member method.
+                    else
+                    {
+                        $class .= " function ";
+                        if( strcmp($words[$index], $className) == 0 )
+                        {
+                            $class .= "__construct";
+                        }
+                        else if( strcmp($words[$index], "~".$className) == 0 )
+                        {
+                            $class .= "__destruct";
+                        }
+                        else
+                        {
+                            $class .= $words[$index];
+                        }
+                        $index++;
+                        $localVariable = [];
 
-                      $class .= $value;
-                      break;
-                  }
-                  $class .= $header . " function ";
-  
-                  // member method.
-                  if( strcmp($words[1], $className) == 0 )
-                  {
-                      $class .= "__construct";
-                  }
-                  else if( strcmp($words[1], "~".$className) == 0 )
-                  {
-                      $class .= "__destruct";
-                  }
-                  else
-                  {
-                      $class .= $words[1];
-                  }
-                  $localVariable = [];
-  
-                  $class .= "(";
-                  for ($i = 2; $i <$wordsLength; $i++)
-                  {
-                      $localVariable[] = $words[$i];
-                      if( $i > 2 )
-                      {
-                          $class .= ",";
-                      }
-                      $class .= "$" . $words[$i];
-                  }
-                  $class .= ")";
-                  break;
-              default:
-                  for ($i = 0; $i<count($localVariable); $i++)
-                  {
-                      $value = preg_replace(
-                          '/([\s,();=])('.$localVariable[$i].')([\s,();=])/', "$1 $" . $localVariable[$i]. " $3", $value);
-                  }
-                  for ($i = 0; $i<count($memberVariable); $i++)
-                  {
-                      $value = preg_replace(
-                          '/([\s,();=])('.$memberVariable[$i].')([\s,();=])/',
-                            "$1 " . '$this->' . $memberVariable[$i] . " $3", $value);
-                  }
-                  $class .= $value;
-                  break;
-          }
-      }
-  }
-  eval( $class );
+                        for ($i = $index; $i <$wordsLength; $i++)
+                        {
+                            if( strcmp(",", $words[$i]) != 0 && 
+                                strcmp("(", $words[$i]) != 0 &&
+                                strcmp(")", $words[$i]) != 0 )
+                            {
+                                $localVariable[] = $words[$i];
+                                $class .= "$";
+                            }
+                            $class .= $words[$i];
+                        }
+                    }
+                    break;
+                default:                
+                    for ($i = 0; $i <$wordsLength; $i++)
+                    {
+                        if( strcmp($words[$i],"var") == 0 )
+                        {
+                            $localVariable[] = $words[$i+1];
+                        }
+                        else if( strcmp($words[$i],"as") == 0 )
+                        {
+                            $class .= " as ";
+                            $localVariable[] = $words[$i+1];
+                        }
+                        else if( strcmp($words[$i],"return") == 0 )
+                        {
+                            $class .= "return ";
+                        }                        
+                        else if( strcmp($words[$i],"new") == 0 )
+                        {
+                            $class .= "new ";
+                        }
+                        else
+                        {
+                            $doll = "";
+                            for ($j = 0; $j<count($localVariable); $j++)
+                            {
+                                if( strcmp($words[$i],$localVariable[$j]) == 0 )
+                                {
+                                    $doll = "$";
+                                    break;
+                                }
+                            }
+                            if( strcmp($doll, "") == 0 )
+                            { 
+                                for ($j = 0; $j<count($memberVariable); $j++)
+                                {
+                                    if( strcmp($words[$i],$memberVariable[$j]) == 0 )
+                                    {
+                                        $doll = '$this->';
+                                        break;
+                                    }
+                                }
+                            }
+                            $class .= $doll . $words[$i];
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+    eval( $class );
 }
 ?>
